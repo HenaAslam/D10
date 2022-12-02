0315d3470749e9d9ce45952562b7eb883702c26b
ajeer333 <ajeer333@gmail.com>
2022-12-02 15:33:49 +0100

    Update solo.js

--- solo.js
+++ solo.js
@@ -38,13 +38,13 @@ delete me.age;
 /* EXERCISE F
 
 Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
-
+me.skills=["HTML","CSS","JavaScript"];
 */
 
 /* EXERCISE G
 
 Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
-
+me.skills.pop();
 */
 
 // JS Functions
@@ -52,13 +52,29 @@ Write a piece of code for programmatically removing the last skill from the skil
 /* EXERCISE 1
 
 Write a function called dice; it should randomize an integer number between 1 and 6.
+function dice(n1,n2){
+    return Math.floor(Math.random()*(n2 - n1+ 1) + n1);
+
+}
+random=dice(1,6);
+console.log(`The randomnumber is ${random}`);
 
 */
 
 /* EXERCISE 2
 
 Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
+function whoIsBigger(n1,n2){
+    if(n1>=n2){
+        return n1;
+    }
+    else{
+        return n2;
+    }
 
+}
+big=whoIsBigger(8,5);
+console.log(`The bigger number is ${big}`);
 */
 
 /* EXERCISE 3
@@ -66,6 +82,10 @@ Write a function called whoIsBigger which receives 2 numbers as parameters and r
 Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
 
 Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
+function splitMe(arr){
+    return arr.split(" ");
+}
+console.log("The split array is", splitMe("hello world this is hena"));
 
 */
 
@@ -74,7 +94,16 @@ Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
 Write a function called deleteOne which receives a string and a boolean as parameters.
 
 If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
-
+function deleteOne(str1,boo1){
+    if(boo1===true){
+        return str1.slice(1)
+    }
+    else{
+        return str1.slice(0,-1)
+    }
+}
+resultString=deleteOne("hello hena",false);
+console.log(`The result string is ${resultString}`);
 */
 
 /* EXERCISE 5
@@ -83,18 +112,59 @@ Write a function called onlyLetters which receives a string as a parameter and r
 
 Ex.: onlyLetters("I have 4 dogs") => returns "I have dogs"
 
+function onlyLetters(str){
+   return str.replace(/[0-9]/g, '')
+}
+
+let digitRemoved=onlyLetters(" i have 4 dogs and 23 cats");
+console.log(`The string without digits is ${digitRemoved}`)
+
 */
 
 /* EXERCISE 6
 
 Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
+ function isThisAnEmail(str)
+ {
+    let atSymbol = str.indexOf("@");
+    let dot = str.indexOf(".");
+    if(atSymbol < 1 || dot === str.length - 1){
+     return false;
+    }
+    else{
+        return true;
+    }
 
+ }
+
+ let check=isThisAnEmail("helloo.hena@gmail.com");
+ console.log(`Checking if the string is an email: ${check}`)
 */
 
 /* EXERCISE 7
 
 Write a function called whatDayIsIt that should return the current day of the week.
+function whatDayIsIt(){
 
+    let today = new Date('December 2, 2022 23:15:30');
+    let day1 = today.getDay();
+    return day1;
+ }
+ let weekDay=whatDayIsIt();
+ if(weekDay===0)
+ console.log("Sunday");
+ if(weekDay===1)
+ console.log("Monday");
+ if(weekDay===2)
+ console.log("Tuesday");
+ if(weekDay===3)
+ console.log("Wednesday");
+ if(weekDay===4)
+ console.log("Thursday");
+ if(weekDay===5)
+ console.log("Friday");
+ if(weekDay===6)
+ console.log("Saturday");
 */
 
 /* EXERCISE 8
@@ -138,6 +208,16 @@ Write a function called isTodayMyBirthday which should return true if today’s 
 Write a function called deleteProp which receives an object and a string as parameters,
 
 and returns the given object after deleting its property named as the given string.
+function deleteProp(obj,str){
+    let wordToSearch=str;
+    delete obj[wordToSearch];
+    return obj;
+ }
+
+
+ let obj1={name:"hena", surname:"asslam", discord:"@hena"}
+ console.log("After deleting given property:",deleteProp(obj1,"surname"))
+ 
 
 */
 
@@ -145,41 +225,119 @@ and returns the given object after deleting its property named as the given stri
 
 Write a function called oldestMovie which finds the oldest movie in the provided movies array.
 
+let randYear=9999
+let movieName;
+function oldestMovie(arr){
+    for(i=0;i<arr.length;i++){
+       let movie=arr[i];
+       if(movie.Year<randYear){
+        randYear=movie.Year;
+        movieName=movie.Title;
+        
+       }
+
+        }
+        return movieName;
+    }
+
+console.log("The oldest movie in the list is:",oldestMovie(movies));
 */
 
 /* EXERCISE 13
 
 Write a function called countMovies which returns the number of movies contained in the provided movies array.
-
+function countMovies(arr){
+    return arr.length;
+}
+console.log("Number of movies in the list is:",countMovies(movies));
 */
 
 /* EXERCISE 14
 
 Write a function called onlyTheTitles which creates an array with just the titles of the movies contained in the provided movies array.
+newArray=[];
+function onlyTheTitles(arr){
+    for(i=0;i<arr.length;i++){
+        let movie=arr[i];
+        newArray.push(movie.Title);
+    }
+    return newArray
 
+}
+console.log(onlyTheTitles(movies))
 */
 
 /* EXERCISE 15
 
 Write a function called onlyInThisMillennium which returns only the movies produced in this millennium from the provided movies array.
+milArray=[];
+function onlyInThisMillennium(arr){
+    for(i=0;i<arr.length;i++){
+        let movie=arr[i];
+        if(movie.Year>=2001)
+        {
+            milArray.push(movie.Title);
+        }
 
+    }
+    return milArray
+}
+console.log("The movies produced in this millenium:", onlyInThisMillennium(movies))
 */
-
 /* EXERCISE 16
 
 Write a function called getMovieById which receives an id as a parameter and returns the movie with the given id from the provided movies array.
+function getMovieById(arr,id){
+    for(i=0;i<arr.length;i++){
+        let movie=arr[i];
+        if(movie.imdbID===id){
+            return movie;
+            
+        
+        }
+    }
+}
 
+console.log("The movie with given id: ",getMovieById(movies,"tt4154796"))
 */
 
 /* EXERCISE 17
 
 Write a function called sumAllTheYears which returns the sum of all the years in which the movies in the provided movies array have been produced.
 
+let yearSum=0;
+function sumAllTheYears(arr){
+    for(i=0;i<arr.length;i++){
+        let movie=arr[i];
+        yearSum=yearSum+parseInt(movie.Year);
+
+            }
+            return yearSum;
+    }
+console.log("Sum of all years",sumAllTheYears(movies));
 */
 
 /* EXERCISE 18
 
 Write a function called searchByTitle which receives a string as a parameter and returns all the movies in the provided movies array which contain that string in the title.
+strArray=[]
+function searchByTitle(arr,str){
+    for(i=0;i<arr.length;i++){
+        movie=arr[i];
+        let word=movie.Title.split(" ");
+        
+        for(j=0;j<word.length;j++){
+            if(word[j]===str){
+                strArray.push(movie.Title);
+            }
+        }
+    }
+    return strArray
+    
+    
+}
+
+console.log(searchByTitle(movies,"Avengers:"))
 
 */
 
@@ -191,12 +349,45 @@ this object should contain an array called match, made by all the movies from th
 
 and another array unmatch with all the remaining ones.
 
+objDiv={match: [], unmatch:[]};
+function searchAndDivide(arr,str){
+        for(i=0;i<arr.length;i++){
+            let movie=arr[i];
+            let word=movie.Title.split(" ");
+            for(j=0;j<word.length;j++){
+                if(word[j]===str){
+                    objDiv.match.push(movie.Title);
+            
+                }
+             
+            }
+            
+            
+        }
+        
+
+        return objDiv;
+
+
+}
+console.log(searchAndDivide(movies,"Lord"))
+
 */
 
 /* EXERCISE 20
 
 Write a function called "removeIndex" which receives a number as a parameter and returns the provided movies array without the element in the given position.
+function removeIndex(arr,n){
+    for(i=0;i<arr.length;i++){
+        
+        if(i===n){
+            arr.splice(n,1);
+        }
 
+    }
+    return arr;
+}
+console.log("Removing element of position given",removeIndex(movies,1))
 */
 
 // [EXTRAS] JS Advanced
